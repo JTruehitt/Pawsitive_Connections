283353f679fe6660ae9fbfb5e802b6d9d9dcdfcb
LaiSha Buck <laishabuck@gmail.com>
2023-06-03 16:21:36 -0400

    changes to home.handlebars

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,19 @@
 (console.log("Successfully linked."))()
 
-const menuToggle = document.querySelector('.headerBar');
+const navBar = document.querySelector('.headerBar');
 const navLinks = document.querySelector('.links');
 
 
-menuToggle.addEventListener('click', () => {
-   menuToggle.classList.toggle('active');
-  navLinks.classList.toggle('active');
+navBar.addEventListener('click', () => {
+   navBar.classList.toggle('active');
+  navList.classList.toggle('active');
 });
 
 const aboutUs = document.getElementById(".aboutUs");
 
 aboutUs.addEventListener('click', function(event){
   event.preventDefault();
-  window.location.href = 'aboutus.html';
+  window.location.href = 'aboutus.handlebars';
 });
 window.onload = function() {
   var statusCode = window.location.search.split('=')[1];
